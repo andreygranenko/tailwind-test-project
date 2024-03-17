e25f34cd4166d0782be42a3259821c9163608ec9
andreygranenko <131700684+andreygranenko@users.noreply.github.com>
2024-03-17 18:59:23 +0200

    added project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+
+    "./public/index.html" /*-- replace this with your path to your html file*/
+]
+
+,
+theme: {
+  extend: {},
+},
+plugins: [],
+}
